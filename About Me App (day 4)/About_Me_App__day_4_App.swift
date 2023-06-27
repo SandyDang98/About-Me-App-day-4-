af505d7f31680aa53a4ec395a1e7616583e6dd17
Sandy Dang <sandy.dang371@gmail.com>
2023-06-27 09:18:31 -0600

    Initial Commit

---
+++ About Me App (day 4)/About_Me_App__day_4_App.swift
@@ -0,0 +1,17 @@
+//
+//  About_Me_App__day_4_App.swift
+//  About Me App (day 4)
+//
+//  Created by Scholar on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct About_Me_App__day_4_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ About Me App (day 4)/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  About Me App (day 4)
+//
+//  Created by Scholar on 6/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var name = ""
+    @State private var textTitle = "I'm a rasing sophomore at Timberline High School. I speak Vietnamese and English at home. I have lived in Idaho my whole life"
+    var body: some View {
+        ZStack {
+            Color(.systemCyan)
+                .ignoresSafeArea()
+            
+            VStack (alignment: .leading, spacing: 20.0){
+                Image("beachpic")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(20)
+                
+                HStack(spacing: 20.0){
+                    Text("Sandy Dang")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                    
+                    Button("Fun Fact") {
+                    textTitle = " I like to go to the beach"
+                                        }
+                    .font(.title2)
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color(hue: 0.597, saturation: 0.588, brightness: 0.694))
+                    
+                }
+                Text(textTitle)
+
+                
+            }
+            
+            .padding()
+            .background(Rectangle().foregroundColor(Color.white))
+            .cornerRadius(15)
+            .shadow(radius : 15)
+            .padding()
+        }
+        
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
